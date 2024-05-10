837ec4ccc52ff496b77f78b4fce8b51a86466ad5
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-05-10 15:16:53 -0400

    Create upload-imgur.sh

---
+++ .local/scripts/tools/upload-imgur.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+notify-send -t 2000 "Uploading to Imgur" &
+
+# Check if file is specified
+file="$1"
+if [ -z "${file}" ]; then
+    notify-send "Error - no file specified" >&2
+    exit 1
+fi
+
+# Imgur upload YOU HAVE TO GIVE YOUR OWN CLIENT_ID
+IMGUR_CLIENT_ID="REPLACETHIS"
+response=$(curl -s -F "image=@${file}" -H "Authorization: Client-ID ${IMGUR_CLIENT_ID}" https://api.imgur.com/3/upload)
+
+# Extract URL
+url=$(echo "${response}" | grep -Po '"link":"\K[^"]+' | sed 's/\\//g')
+
+# Copy to clipboard and notify
+echo -n "${url}" | xclip -selection clipboard
+notify-send -t 2000 "Image URL Copied to Clipboard: ${url}"
