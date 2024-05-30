4f0c00049fff9eb10d52f790839f29656fe1aee7
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-05-30 08:52:45 -0400

    Update kdeconnect-anyfm.sh

--- .local/scripts/tools/kdeconnect-anyfm.sh
+++ .local/scripts/tools/kdeconnect-anyfm.sh
@@ -29,28 +29,31 @@ else
 	exit 1
 fi
 
+_link_phone() { 
+	# Get paths
+	PHONEPATHS=$("$QDBUS" org.kde.kdeconnect /modules/kdeconnect/devices/"$PHONEID"/sftp getDirectories 2>/dev/null)
+	PHONEPATH1=$(echo "$PHONEPATHS" | awk -F ": " 'NR==1 {print $1}')
+	PHONEPATH2=$(echo "$PHONEPATHS" | awk -F ": " 'NR==2 {print $1}')
+	DIRNAME1=$(echo "$PHONEPATHS" | awk -F ": " 'NR==1 {print $2}')
+	DIRNAME2=$(echo "$PHONEPATHS" | awk -F ": " 'NR==2 {print $2}')
+	# Make and link dirs
+	mkdir -p "$PHONEDIR"
+	[ -e "$PHONEPATH1" ] && ln -s "$PHONEPATH1" "$PHONEDIR/$DIRNAME1" 2>/dev/null
+	[ -e "$PHONEPATH2" ] && ln -s "$PHONEPATH2" "$PHONEDIR/$DIRNAME2" 2>/dev/null
+	ln -s "$PHONEDIR" "$HOME" 2>/dev/null
+}
+
 # MOUNT PHONE
 while true; do
-	PHONEID=$(kdeconnect-cli -a --id-name-only 2>/dev/null | awk '{print $1}')
-	if [ -z "$PHONEID" ]; then
-		echo "No phone connected"
-	else
-		"$QDBUS" org.kde.kdeconnect /modules/kdeconnect/devices/"$PHONEID"/sftp mountAndWait
-		mount | grep kdeconnect && { echo "Phone mounted"; break; }
+	if ! mount | grep kdeconnect; then
+		find "$PHONEDIR" -xtype l -delete
+		PHONEID=$(kdeconnect-cli -a --id-name-only 2>/dev/null | awk '{print $1}')
+		if [ -z "$PHONEID" ]; then
+			echo "No phone connected"
+		else
+			"$QDBUS" org.kde.kdeconnect /modules/kdeconnect/devices/"$PHONEID"/sftp mountAndWait
+			mount | grep kdeconnect && echo "Phone mounted" && _link_phone
+		fi
 	fi
-	sleep 5
+	sleep 6
 done
- 
-# Get paths
-PHONEPATHS=$("$QDBUS" org.kde.kdeconnect /modules/kdeconnect/devices/"$PHONEID"/sftp getDirectories 2>/dev/null)
-PHONEPATH1=$(echo "$PHONEPATHS" | awk -F ": " 'NR==1 {print $1}')
-PHONEPATH2=$(echo "$PHONEPATHS" | awk -F ": " 'NR==2 {print $1}')
-DIRNAME1=$(echo "$PHONEPATHS" | awk -F ": " 'NR==1 {print $2}')
-DIRNAME2=$(echo "$PHONEPATHS" | awk -F ": " 'NR==2 {print $2}')
-
-# Make and link dirs
-mkdir -p "$PHONEDIR"
-find "$PHONEDIR" -xtype l -delete # Clears broken links
-[ -e "$PHONEPATH1" ] && ln -s "$PHONEPATH1" "$PHONEDIR/$DIRNAME1" 2>/dev/null
-[ -e "$PHONEPATH2" ] && ln -s "$PHONEPATH2" "$PHONEDIR/$DIRNAME2" 2>/dev/null
-ln -s "$PHONEDIR" "$HOME" 2>/dev/null
