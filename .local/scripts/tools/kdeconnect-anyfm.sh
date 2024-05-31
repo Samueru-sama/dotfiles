2384c5d09238bf5493d2cbe470d8504b15c2a276
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-05-30 23:23:51 -0400

    Update kdeconnect-anyfm.sh

--- .local/scripts/tools/kdeconnect-anyfm.sh
+++ .local/scripts/tools/kdeconnect-anyfm.sh
@@ -29,30 +29,36 @@ else
 	exit 1
 fi
 
-_link_phone() { 
-	# Get paths
+_mount_and_link_phone() {
+	if ! mount | grep kdeconnect >/dev/null 2>&1; then
+		"$QDBUS" org.kde.kdeconnect /modules/kdeconnect/devices/"$PHONEID"/sftp mountAndWait \
+		|| { notify-send -u critical "Kdeconnect failed to mount phone"; exit 1; }
+		notify-send "Phone connected and mounted"
+	fi
+	
 	PHONEPATHS=$("$QDBUS" org.kde.kdeconnect /modules/kdeconnect/devices/"$PHONEID"/sftp getDirectories 2>/dev/null)
-	PHONEPATH1=$(echo "$PHONEPATHS" | awk -F ": " 'NR==1 {print $1}')
-	PHONEPATH2=$(echo "$PHONEPATHS" | awk -F ": " 'NR==2 {print $1}')
-	DIRNAME1=$(echo "$PHONEPATHS" | awk -F ": " 'NR==1 {print $2}')
-	DIRNAME2=$(echo "$PHONEPATHS" | awk -F ": " 'NR==2 {print $2}')
-	# Make and link dirs
-	mkdir -p "$PHONEDIR"
-	[ -e "$PHONEPATH1" ] && ln -s "$PHONEPATH1" "$PHONEDIR/$DIRNAME1" 2>/dev/null
-	[ -e "$PHONEPATH2" ] && ln -s "$PHONEPATH2" "$PHONEDIR/$DIRNAME2" 2>/dev/null
-	ln -s "$PHONEDIR" "$HOME" 2>/dev/null
+	if [ -n "$PHONEPATHS" ]; then
+		PHONEPATH1=$(echo "$PHONEPATHS" | awk -F ": " 'NR==1 {print $1}')
+		PHONEPATH2=$(echo "$PHONEPATHS" | awk -F ": " 'NR==2 {print $1}')
+		DIRNAME1=$(echo "$PHONEPATHS" | awk -F ": " 'NR==1 {print $2}')
+		DIRNAME2=$(echo "$PHONEPATHS" | awk -F ": " 'NR==2 {print $2}')
+
+		mkdir -p "$PHONEDIR"
+		[ -e "$PHONEPATH1" ] && ln -s "$PHONEPATH1" "$PHONEDIR/$DIRNAME1" 2>/dev/null
+		[ -e "$PHONEPATH2" ] && ln -s "$PHONEPATH2" "$PHONEDIR/$DIRNAME2" 2>/dev/null
+		ln -s "$PHONEDIR" "$HOME" 2>/dev/null
+	fi
 }
 
-# MOUNT PHONE
+# CHECK AND MOUNT PHONE
 while true; do
-	if ! mount | grep kdeconnect; then
+	if ! mount | grep kdeconnect >/dev/null 2>&1 || [ -z "$(ls "$PHONEDIR" 2>/dev/null)" ]; then
 		find "$PHONEDIR" -xtype l -delete
 		PHONEID=$(kdeconnect-cli -a --id-name-only 2>/dev/null | awk '{print $1}')
-		if [ -z "$PHONEID" ]; then
-			echo "No phone connected"
+		if [ -n "$PHONEID" ]; then
+			_mount_and_link_phone
 		else
-			"$QDBUS" org.kde.kdeconnect /modules/kdeconnect/devices/"$PHONEID"/sftp mountAndWait
-			mount | grep kdeconnect && echo "Phone mounted" && _link_phone
+			echo "Phone not connected"
 		fi
 	fi
 	sleep 6
