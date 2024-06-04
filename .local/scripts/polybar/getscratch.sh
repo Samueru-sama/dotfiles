488d16c7c07876afee852c29d5cb2315148fba99
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-06-04 10:20:14 -0400

    Update getscratch.sh

--- .local/scripts/polybar/getscratch.sh
+++ .local/scripts/polybar/getscratch.sh
@@ -3,6 +3,6 @@
 # SCRATCHPAD INDICATOR FOR POLYBAR
 
 while true; do
-	i3-msg -t get_tree | LC_ALL=C awk 'BEGIN{RS="\""} /fresh|changed/ {count++} END{print count}'
+	i3-msg -t get_tree | LC_ALL=C awk 'BEGIN{RS=":"} /"fresh"|"changed"/ {count++} END{print count}'
 	sleep 0.7
 done
