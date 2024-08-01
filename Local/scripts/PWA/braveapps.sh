7e97eaea196fe3e89552669a6e870ff9851b38b0
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-07-31 20:33:52 -0400

    Update braveapps.sh

--- Local/scripts/PWA/braveapps.sh
+++ Local/scripts/PWA/braveapps.sh
@@ -4,35 +4,42 @@ if ! command -v brave 1>/dev/null || ! command -v xdotool 1>/dev/null; then
 	echo "You need Brave Browser and xdotool for this to work"
 	notify-send "Missing dependency"
 	exit 1
+elif [ -z "$@" ]; then
+	echo "No PWA given, bailing out"
+	echo "Options are: discord - telegram - whatsapp - piped"
+	exit 1
 fi
 
-case args in "$@"
-	discord)
-		i3-msg '[class="Brave" instance="discord.com__app"] focus' 2>/dev/null || brave --app=https://discord.com/app &
-		;;
-		
-	whatsapp)
-		i3-msg '[class="Brave" instance="web.whatsapp.com"] focus' 2>/dev/null || brave --app=https://web.whatsapp.com &
-		;;
+for arg in "$@"; do
+	case "$arg" in
+		discord)
+			i3-msg '[class="Brave" instance="discord.com__app"] focus' 2>/dev/null || brave --app=https://discord.com/app &
+			;;
+			
+		whatsapp)
+			i3-msg '[class="Brave" instance="web.whatsapp.com"] focus' 2>/dev/null || brave --app=https://web.whatsapp.com &
+			;;
 
-	telegram)
-		i3-msg '[class="Brave" instance="web.telegram.org__k"] focus' 2>/dev/null || brave --app=https://web.telegram.org/k &
-		;;
+		telegram)
+			i3-msg '[class="Brave" instance="web.telegram.org__k"] focus' 2>/dev/null || brave --app=https://web.telegram.org/k &
+			;;
 
-	piped)
-		VID=$(echo "$@" | awk -F / '{print $NF}')
-		brave --app="https://piped.kavin.rocks/$VID" --window-name="Piped" &
-		exit 0
-		;;
-esac
+		piped)
+			VID=$(echo "$@" | awk -F / '{print $NF}')
+			brave --app="https://piped.kavin.rocks/$VID" --window-name="Piped" &
+			exit 0
+			;;
+	esac
+done
 
 if ! xdotool search --name "telegram" || ! xdotool search --name "discord" || ! xdotool search --name "whatsapp"; then
 	notify-send "Opening chat applications"
 	COUNT=0
 	while [ $COUNT -ne 100 ]; do		# THIS IS A HACK BECAUSE OF THIS https://github.com/i3/i3/issues/5916
-		i3-msg '[class="Brave" instance="web.telegram.org__k"] focus' && i3-msg layout tabbed && break
+		i3-msg '[class="Brave" instance="web.telegram.org__k"] focus' >/dev/null 2>&1 \
+			&& i3-msg layout tabbed >/dev/null 2>&1 \
+			&& break
 		COUNT=$((COUNT + 1))
 		sleep 0.2
 	done
 fi
-
