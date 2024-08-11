ba4e510911042ff645baf3963658b667d0bdc51f
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-08-10 22:24:09 -0400

    Update braveapps.sh

--- Local/scripts/PWA/braveapps.sh
+++ Local/scripts/PWA/braveapps.sh
@@ -4,7 +4,7 @@ if ! command -v brave 1>/dev/null || ! command -v xdotool 1>/dev/null; then
 	echo "You need Brave Browser and xdotool for this to work"
 	notify-send "Missing dependency"
 	exit 1
-elif [ -z "$@" ]; then
+elif [ -z "$@" 2>/dev/null ]; then
 	echo "No PWA given, bailing out"
 	echo "Options are: discord - telegram - whatsapp - piped"
 	exit 1
@@ -28,8 +28,8 @@ for arg in "$@"; do
 			;;
 
 		piped)
-			VID=$(echo "$@" | grep "http" | awk -F / '{print $NF}')
-			brave --app="https://piped.kavin.rocks/$VID" --window-name="Piped" &
+			VID=$(echo "$@" | grep -o "http[^ ]*" | awk -F / '{print $NF}')
+			brave --app="https://piped.kavin.rocks/$VID" --window-name="Piped"
 			exit 0
 			;;
 	esac
@@ -39,11 +39,11 @@ if ! xdotool search --name "telegram" || ! xdotool search --name "discord" \
 	|| ! xdotool search --name "whatsapp"; then
 	notify-send "Opening chat applications"
 	COUNT=0
-	while [ $COUNT -ne 100 ]; do # THIS IS A HACK BECAUSE OF THIS https://github.com/i3/i3/issues/5916
+	while [ $COUNT -lt 100 ]; do # THIS IS A HACK BECAUSE OF THIS https://github.com/i3/i3/issues/5916
 		i3-msg '[class="Brave" instance="web.telegram.org__k"] focus' >/dev/null 2>&1 \
 			&& i3-msg layout tabbed >/dev/null 2>&1 \
 			&& break
 		COUNT=$((COUNT + 1))
-		sleep 0.2
+		sleep 0.1
 	done
 fi
