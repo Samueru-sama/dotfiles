af011d072dd6dacc7cae8d89ed7a3b19b9e2d3b7
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-08-17 04:50:51 -0400

    Update samueru.lua

--- Local/config/lite-xl/colors/samueru.lua
+++ Local/config/lite-xl/colors/samueru.lua
@@ -1,38 +1,46 @@
-local style = require "core.style"
-local common = require "core.common"
+local style      = require "core.style"
+local common     = require "core.common"
+local dark_blue  = { common.color "#091927" }
+local blue       = { common.color "#175178" }
+local light_blue = { common.color "#05e6fa" }
+local red        = { common.color "#cb4c51" }
+local gray_blue  = { common.color "#707282" }
+local light_gray = { common.color "#b2c8df" }
+local white      = { common.color "#d4d9d6" }
+local yellow     = { common.color "#F0C674" }
 
 -- App --
-style.background = { common.color "#04090e" }
-style.background2 = { common.color "#091f2e" }
-style.background3 = { common.color "#091f2e" }
-style.text = { common.color "#F0C674" }
-style.caret = { common.color "#F0C674" }
-style.accent = { common.color "#ffb86c" }
-style.dim = { common.color "#707282" }
-style.divider = { common.color "#22242e" }
-style.selection = { common.color "#4c5163" }
-style.line_number = { common.color "#44475a" }
-style.line_number2 = { common.color "#717796" }
-style.line_highlight = { common.color "#2d303d" }
-style.scrollbar = { common.color "#44475a" }
-style.scrollbar2 = { common.color "#4c5163" }
+style.background     = { common.color "#04090e" }
+style.background2    = { common.color "#091f2e" }
+style.background3    = { common.color "#091f2e" }
+style.text           = white
+style.caret          = yellow
+style.accent         = white
+style.dim            = gray_blue
+style.divider        = yellow
+style.selection      = blue
+style.line_number    = gray_blue
+style.line_number2   = red
+style.line_highlight = dark_blue
+style.scrollbar      = yellow
+style.scrollbar2     = yellow
 
 -- Syntax --
 style.syntax = {}
-style.syntax["normal"] = { common.color "#f5faff" }
-style.syntax["symbol"] = { common.color "#F0C674" }
-style.syntax["comment"] = { common.color "#707282" }
-style.syntax["keyword"] = { common.color "#F0C674" }
-style.syntax["keyword2"] = { common.color "#05e6fa" }
-style.syntax["number"] = { common.color "#cb4c51" }
-style.syntax["literal"] = { common.color "#cb4c51" }
-style.syntax["string"] = { common.color "#b2c8df" }
-style.syntax["operator"] = { common.color "#F0C674" }
-style.syntax["function"] = { common.color "#cb4c51" }
+style.syntax["normal"]   = white
+style.syntax["symbol"]   = light_blue
+style.syntax["comment"]  = gray_blue
+style.syntax["keyword"]  = yellow
+style.syntax["keyword2"] = light_blue
+style.syntax["number"]   = red
+style.syntax["literal"]  = red
+style.syntax["string"]   = light_gray
+style.syntax["operator"] = yellow
+style.syntax["function"] = red
 
 -- Lint+ --
-style.lint               = {}
-style.lint["info"]       = { common.color "#448bf5" }
-style.lint["hint"]       = { common.color "#47e2b1" }
-style.lint["warning"]    = { common.color "#f5ad44" }
-style.lint["error"]      = { common.color "#db504a" }
+style.lint            = {}
+style.lint["info"]    = white
+style.lint["hint"]    = yellow
+style.lint["warning"] = red
+style.lint["error"]   = red
