68e3b59bb78ffa4cbc3e5ff17ab6b34d8fbb4e04
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-09-19 15:07:36 -0400

    Update pacmanhog.sh

--- Local/scripts/tools/pacmanhog.sh
+++ Local/scripts/tools/pacmanhog.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 # Similar to xhog in voidlinux
-pacman -Qi | awk -F': ' '/Name/ {name=$2} /Installed Size/ {size=$2} name && size {print name, size; name=size=""}' | column -t | grep MiB | sort -nk 2
-
+pacman -Qi | awk -F': ' '/Name/ {name=$2}
+			/Installed Size/ {size=$2}
+			name && size {print name, size; name=size=""}' \
+			  | column -t | grep MiB | sort -nk 2
