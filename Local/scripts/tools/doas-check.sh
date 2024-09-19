5a8aef91506e329cc681b5cc1b2f2d8f6ca55244
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-09-19 15:04:35 -0400

    Update doas-check.sh

--- Local/scripts/tools/doas-check.sh
+++ Local/scripts/tools/doas-check.sh
@@ -4,14 +4,24 @@ BINDIR="${XDG_BIN_HOME:-$HOME/.local/bin}"
 DOASCONF="${XDG_CONFIG_HOME:-$HOME/.config}/doas/doas.conf"
 SUDO=$(command -v pkexec || command -v sudo)
 
-cat "$DOASCONF" 1>/dev/null || { echo "MISSING $DOASCONF !" & notify-send -u critical "MISSING $DOASCONF"; exit 1; }
-
+if [ ! -f "$DOASCONF" ]; then
+	echo "MISSING \"$DOASCONF\"!"
+	notify-send -u critical "MISSING $DOASCONF"
+	exit 1
+fi
 if [ "$(cat /etc/doas.conf)" != "$(cat "$DOASCONF")" ]; then
 	"$SUDO" cp "$DOASCONF" /etc/doas.conf
 	"$SUDO" chown root /etc/doas.conf
 fi
 
-ls /usr/local/bin/iotop  1>/dev/null || ( ls "$BINDIR"/iotop  && "$SUDO" ln -s "$BINDIR"/iotop  /usr/local/bin )
-ls /usr/local/bin/ps_mem 1>/dev/null || ( ls "$BINDIR"/ps_mem && "$SUDO" ln -s "$BINDIR"/ps_mem /usr/local/bin )
-ls /usr/local/bin/zramen 1>/dev/null || ( ls "$BINDIR"/zramen && "$SUDO" ln -s "$BINDIR"/zramen /usr/local/bin )
+if [ ! -L /usr/local/bin/iotop ]; then
+	[ -f "$BINDIR"/iotop ] && "$SUDO" ln -s "$BINDIR"/iotop  /usr/local/bin
+fi
 
+if [ ! -L /usr/local/bin/ps_mem ]; then
+	[ -f "$BINDIR"/ps_mem ] && "$SUDO" ln -s "$BINDIR"/ps_mem /usr/local/bin
+fi
+
+if [ ! -L /usr/local/bin/zramen ]; then
+	[ -f "$BINDIR"/zramen ] && "$SUDO" ln -s "$BINDIR"/zramen /usr/local/bin
+fi
