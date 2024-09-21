638a72d06f0d221d1bc454bf3ee6b7e564826f58
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-09-21 02:43:58 -0400

    Update dbin-wrapper.sh

--- Local/scripts/tools/dbin-wrapper.sh
+++ Local/scripts/tools/dbin-wrapper.sh
@@ -4,33 +4,32 @@
 
 DBIN="$XDG_SBIN_HOME/dbin"
 if [ ! -e "$DBIN" ]; then
-  echo "dbin is not installed or this script has the wrong path"
-  exit 1
+	echo "dbin is not installed or this script has the wrong path"
+	exit 1
 fi
 
 case "$1" in
-  '-i')
-    shift
-    "$DBIN" install "$@"
-    ;;
-  '-r')
-    shift
-    "$DBIN" remove "$@"
-    ;;
-  '-l')
-    shift
-    "$DBIN" list "$@"
-    ;;
-  '-u')
-    shift
-    "$DBIN" update "$@"
-    ;;
-  '-q')
-    shift
-    "$DBIN" search "$@"
-    ;;
-  *)
-    "$DBIN" "$@"
-    ;;
+	'-i')
+		shift
+		"$DBIN" install "$@"
+		;;
+	'-r')
+		shift
+		"$DBIN" remove "$@"
+		;;
+	'-l')
+		shift
+		"$DBIN" list "$@"
+		;;
+	'-u')
+		shift
+		"$DBIN" update "$@"
+		;;
+	'-q')
+		shift
+		"$DBIN" search "$@"
+		;;
+	*)
+		"$DBIN" "$@"
+		;;
 esac
-
