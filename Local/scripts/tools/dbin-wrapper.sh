3123486986985ea9a304bffebf61d5d7fba57c6d
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-09-21 02:39:59 -0400

    Add files via upload

---
+++ Local/scripts/tools/dbin-wrapper.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# can't get used to dbin flags lol
+
+DBIN="$XDG_SBIN_HOME/dbin"
+if [ ! -e "$DBIN" ]; then
+  echo "dbin is not installed or this script has the wrong path"
+  exit 1
+fi
+
+case "$1" in
+  '-i')
+    shift
+    "$DBIN" install "$@"
+    ;;
+  '-r')
+    shift
+    "$DBIN" remove "$@"
+    ;;
+  '-l')
+    shift
+    "$DBIN" list "$@"
+    ;;
+  '-u')
+    shift
+    "$DBIN" update "$@"
+    ;;
+  '-q')
+    shift
+    "$DBIN" search "$@"
+    ;;
+  *)
+    "$DBIN" "$@"
+    ;;
+esac
+
