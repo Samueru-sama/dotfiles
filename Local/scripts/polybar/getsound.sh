0dcefb1c2a514d643ab7d92c50d93cac5c8e1f4d
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-10-05 11:46:16 -0400

    Update getsound.sh

--- Local/scripts/polybar/getsound.sh
+++ Local/scripts/polybar/getsound.sh
@@ -3,41 +3,74 @@
 # CONTROLS SYSTEM VOLUME IN POLYBAR AND I3
 
 # SAFETY CHECK
-if ! command -v pulsemixer 1>/dev/null || \
-	! command -v pactl 1>/dev/null || ! command -v dunstify 1>/dev/null; then
-	echo "You need pulsemixer, pactl and dunstify for this script to work"
+if ! command -v pactl 1>/dev/null || ! command -v dunstify 1>/dev/null; then
+	echo "You need pamixer, pactl and dunstify for this script to work"
 	notify-send "Missing dependency"
 	exit 1
 fi
 
-# CONTROLS VOLUME THRU POLYBAR
-if [ "$1" = VOL+ ]; then
-	if [ "$2" = --NOLIMIT ]; then # --NOLIMIT ALLOWS VOLUME BEYOND 0dBFS (CLIPPING)
-		pulsemixer --change-volume +5 --unmute --max-volume 145
+vol="$(pactl list sinks | awk '/Volume:/ {printf("%.0f", $7); exit; }')"
+[ "$vol" -gt 10 ] && pactl set-sink-volume @DEFAULT_SINK@ 65% # just in case
+
+_increase_vol() {
+	if [ "$2" = --NOLIMIT ]; then # ALLOWS VOLUME BEYOND 0dBFS (CLIPPING)
+		[ "$vol" -lt 10 ] && pactl set-sink-volume @DEFAULT_SINK@ +5%
 	else
-		pulsemixer --change-volume +5 --unmute --max-volume 100
+		[ "$vol" -lt 0 ] || [ "$vol" = "-inf" ] \
+			&& pactl set-sink-volume @DEFAULT_SINK@ +5%
+		# sometimes the volume ends up being +1dB so we fix it
+		[ "$vol" -gt 0 ] && pactl set-sink-volume @DEFAULT_SINK@ 100%
 	fi
-elif [ "$1" = VOL- ]; then
-	if [ "$2" = --NOLIMIT ]; then
-		pulsemixer --change-volume -5 --unmute --max-volume 145
+}
+
+_decrease_vol() {
+	pactl set-sink-volume @DEFAULT_SINK@ -5%
+}
+
+_toggle_mute() {
+	pactl set-sink-mute @DEFAULT_SINK@ toggle || exit 1
+	mute="$(pactl get-sink-mute @DEFAULT_SINK@ | grep -o 'yes\|no')"
+	if [ "$mute" = "yes" ]; then
+        dunstify -r 11 -t 1000 "Sound: Muted"
+    else
+        dunstify -r 11 -t 1000 "Sound: Unmuted"
+    fi
+}
+
+_notify_vol() {
+	vol="$(pactl list sinks | awk '/Volume:/ {printf("%.0f", $7); exit; }')"
+	if [ "$vol" -gt 0 ]; then
+		dunstify -r 111 -t 1000 "WARNING VOLUME IS OVER $vol dBFS"
+		echo "WARNING VOLUME IS OVER $vol dBFS"
 	else
-		pulsemixer --change-volume -5 --unmute --max-volume 100
+		dunstify -r 11 -t 1000 "Main Vol: $vol dBFS"
+		echo "$vol dBFS"
 	fi
-elif [ "$1" = TOGGLEMUTE ]; then
-	pactl set-sink-mute @DEFAULT_SINK@ toggle && dunstify -r 11 -t 1000 \
-	"$(awk 'BEGIN { cmd = "pactl get-sink-mute @DEFAULT_SINK@"; if ((cmd | getline) > 0) 
-	if (/Mute: yes/) {print "Sound: Muted"; exit} else {print "Sound: Unmuted"; exit}}')"
-	exit 0
-fi
+}
 
-# NOTIFIES VOLUME CHANGES
-CURRENTVOL=$(pactl list sinks | awk '/Volume:/ {printf("%.0f", $7); exit; }')
+_print_help() {
+	me="$(basename $0)"
+	echo " USAGE:"
+	echo " \"$me VOL+\""
+	echo " \"$me VOL-\""
+	echo " \"$me VOL+ --NOLIMIT\" - Allows setting volume beyond 0dBFS"
+}
 
-if [ "$CURRENTVOL" -gt 0 ]; then
-	dunstify -r 111 -t 1000 "WARNING VOLUME IS OVER $CURRENTVOL dBFS"
-	echo "WARNING VOLUME IS OVER $CURRENTVOL dBFS" 
-else
-	dunstify -r 11 -t 1000 "Main Vol: $CURRENTVOL dBFS"
-	echo "$CURRENTVOL dBFS"
-fi
-
+case "$1" in
+	'VOL+')
+		_increase_vol "$@" && _notify_vol
+		;;
+	'VOL-')
+		_decrease_vol && _notify_vol
+		;;
+	'TOGGLEMUTE')
+		_toggle_mute
+		;;
+	'--help')
+		_print_help
+		;;
+	*)
+		_print_help
+		exit 1
+		;;
+esac
