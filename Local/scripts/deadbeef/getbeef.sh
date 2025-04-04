60f2bc74e7bf59c0e4237b9b4b19c03e2a8527d3
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-04-04 02:33:15 -0400

    Update getbeef.sh

--- Local/scripts/deadbeef/getbeef.sh
+++ Local/scripts/deadbeef/getbeef.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
 
 # This script is used to control deadbeef with i3wm
-# Deadbeef has to be configured to hide to tray when closed 
+# Deadbeef has to be configured to hide to tray when closed
 # You can hide the systray icon and it will work still
 
 if ! command -v deadbeef 1>/dev/null || ! command -v playerctl 1>/dev/null \
-|| ! command -v xdo 1>/dev/null || ! command -v xdotool 1>/dev/null; then
+	|| ! command -v xdo 1>/dev/null || ! command -v xdotool 1>/dev/null; then
 	printf "You need deadbeef, xdo, xdotool and playerctl for this script to work"
 	notify-send "Missing dependency"
 	exit 1
 fi
 
 WCLASS=$(xdotool getactivewindow getwindowclassname)
-HACK="--appimage-extract-and-run" # Allows deadbeef launch other appimages when sandboxed by aisap
+tmp_dir="${TMPDIR:-/tmp}/getbeef"
+mkdir -p "$tmp_dir" && chmod -R 700 "$tmp_dir" || exit 1
 
 if [ "$1" = "REPEAT" ]; then
 	if pgrep "deadbeef" 1>/dev/null; then
 		LOOP="$(playerctl loop --appimage-extract-and-run)"
 		if [ "$LOOP" = "None" ]; then
-			playerctl loop Track "$HACK" && notify-send -t 700 "Repeat Track On"
+			playerctl loop Track && notify-send -t 700 "Repeat Track On"
 		else
-			playerctl loop None "$HACK" && notify-send -t 700 "Repeat Track Off"
+			playerctl loop None && notify-send -t 700 "Repeat Track Off"
 		fi
 	else
 		echo "DeaDBeeF is not running"
@@ -30,19 +31,19 @@ elif [ "$1" = "SHUFFLE" ]; then
 	if pgrep "deadbeef" 1>/dev/null; then
 		SHUFFLE=$(playerctl shuffle --appimage-extract-and-run)
 		if [ "$SHUFFLE" = "Off" ]; then
-			playerctl shuffle On "$HACK" && notify-send -t 700 "Shuffle On"
+			playerctl shuffle On && notify-send -t 700 "Shuffle On"
 		else
-			playerctl shuffle Off "$HACK" && notify-send -t 700 "Shuffle Off"
+			playerctl shuffle Off && notify-send -t 700 "Shuffle Off"
 		fi
 	else
 		echo "DeaDBeeF is not running"
 		exit 1
 	fi
 elif [ "$WCLASS" = "Deadbeef" ]; then
-	WID=$(xdo id)
-	xdo hide && echo "$WID" > /tmp/deadbeefid
+	WID="$(xdo id)"
+	xdo hide && echo "$WID" > "$tmp_dir"/deadbeefid
 else
-	WID=$(cat /tmp/deadbeefid)
+	WID=$(cat "$tmp_dir"/deadbeefid)
 	[ -n "$WID" ] && xdo show "$WID" && xdo activate "$WID"
-	pgrep deadbeef 1>/dev/null || { notify-send "Launching DeaDBeef"; exec deadbeef; }
+	pgrep deadbeef || { notify-send "Launching DeaDBeef"; exec deadbeef; }
 fi
